e8ef8aa83be92de3cb682f19b78f05c7cd686509
Valvitor <valvitorscf@gmail.com>
2021-11-14 13:06:56 -0300

    Create bloco.js

---
+++ Início/bloco.js
@@ -0,0 +1,14 @@
+//Aula sobre blóco de códigos
+//Usa-se um par de chaves { } para criar um bloco de códigos
+{
+    console.log("Passo #01")
+    console.log("Passo #02")
+    console.log("Passo #03")
+}
+
+{
+    console.log("Passo #04")
+    console.log("Passo #05")
+}
+
+console.log("Fim!")
