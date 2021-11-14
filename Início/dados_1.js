6848a3fd21d0d98aeb62eb7860efa7c9c0c51ae1
Valvitor <valvitorscf@gmail.com>
2021-11-14 13:16:17 -0300

    Create dados_1.js

---
+++ Início/dados_1.js
@@ -0,0 +1,13 @@
+var quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5;
+var precofinal = preco + imposto;
+var nome = "Caneta"
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+nome = "canetaBIC"
+console.log(precofinal);
+console.log(nome)
