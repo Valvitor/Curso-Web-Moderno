3e8b0ad3fd67674f2340a5fa4dae7a32e6a60485
Valvitor <valvitorscf@gmail.com>
2021-11-14 12:46:24 -0300

    Create comentario.js

---
+++ Início/comentario.js
@@ -0,0 +1,4 @@
+console.log('Passo #01')//Isto é uma linha de comentário
+console.log('Passo #02')//Console.log é usado para exibir algo na tela do computador
+console.log('Passo #03')/*Isto também é um comentário */
+console.log('Passo #N')
