29940d4a1b584e51f3713ac3d218f215f200d590
Valvitor <valvitorscf@gmail.com>
2021-11-15 12:49:42 -0300

    Javascript-Fundamentos

---
+++ 2.Javascript-Fundamentos/destructuring.js
@@ -0,0 +1,18 @@
+//Novo recurso do ES2015
+const pessoa = {
+    nome: 'Ana',
+    idade: 5,
+    endereco:{ 
+        logradouro: 'Rua da Poeira',
+        numero: 772
+    }
+}
+
+const {nome, idade} = pessoa
+console.log(nome +' tem ' + idade + ' anos e precisa de muito carinho!')
+
+const {nome: n, idade: i} = pessoa
+console.log(n, i)
+
+const {sobrenome, bemHumorada = true} = pessoa
+console.log(sobrenome, bemHumorada)
