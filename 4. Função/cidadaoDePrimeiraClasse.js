1e3ce18e3127eb44762c13fb8ed3ed24bf313922
Valvitor <valvitorscf@gmail.com>
2021-11-15 20:46:45 -0300

    Funções

---
+++ 4. Função/cidadaoDePrimeiraClasse.js
@@ -0,0 +1,35 @@
+//Criar uma função de forma literal
+function fun1() {}
+
+//Armazenar uma variável
+
+const fun2 = function () {}
+
+//Armazenar dentro de um Array
+
+const array = [function(a,b){return a + b}, fun1, fun2]
+console.log(array[0](2,3))
+
+//Armazenar em um atributo de objeto
+const obj = {}
+
+obj.falar = function () { return 'Opa'}
+console.log(obj.falar())
+
+//Passar função como parâmetro de outra função
+function run(fun) {
+    fun ()
+}
+
+run(function() {console.log('Executando...')})
+
+//Uma função pode retornar/conter uma outra função
+
+function soma(a, b) {
+    return function (c) {
+        console.log(a+b+c)
+    }
+}
+soma(2,3)(4)
+const cincoMais = soma(2,3)
+cincoMais(4)
